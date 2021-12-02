30d4333a20001f3a97e234263cf4cfe17e0cf2d1
Samuel Selleck <sellecksamuel@gmail.com>
2021-12-02 12:20:33 +0100

    day 1 modification

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,15 +1,15 @@
 use std::fs;
 
 fn main() { 
-    let depths: Vec<f32> = fs::read_to_string("depths.txt")
+    let depths: Vec<_> = fs::read_to_string("depths.txt")
         .expect("file not found.")
         .split('\n')
         .map(|s| s.parse().expect("not a number."))
         .collect();
 
-    let depths_smooth: Vec<f32> = depths
+    let depths_smooth: Vec<_> = depths
         .windows(3)
-        .map(|s| (s[0] + s[1] + s[2])/3.0)
+        .map(|s| s.iter().sum::<f32>()/3.0)
         .collect();
     
     let increases = depths_smooth
