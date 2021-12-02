461dc8173cf644969c128f137d84b5a6c6fa3f9a
Samuel Selleck <sellecksamuel@gmail.com>
2021-12-02 16:28:52 +0100

    day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,19 +3,17 @@ use std::fs;
 fn main() {
     let instructions = fs::read_to_string("instructions.txt").expect("file not found.");
 
-    let commands: Vec<(&str, i32)> = instructions
+    let commands = instructions
         .split('\n')
         .map(|s| s.split_whitespace())
         .map(|mut itr| {
             (
                 itr.next().expect("no command."),
-                itr.next()
-                    .expect("no command argument.")
-                    .parse()
+                itr.next().expect("no command argument.")
+                    .parse::<i32>()
                     .expect("argument not an integer."),
             )
-        })
-        .collect();
+        });
 
     let [mut depth, mut horiz, mut aim] = [0; 3];
     for (cmd, arg) in commands {
