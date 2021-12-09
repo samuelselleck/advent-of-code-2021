6271b8dd49e25e3067fc2bcee76ba40a43c2e146
Samuel Selleck <sellecksamuel@gmail.com>
2021-12-09 15:19:52 +0100

    day 9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,37 +1,71 @@
-use std::fs;
 use std::collections::HashSet;
+use std::fs;
+
+struct Map {
+    terrain: Vec<Vec<u8>>,
+    dims: (i32, i32),
+}
+
+impl Map {
+    fn new(terrain: Vec<Vec<u8>>) -> Map {
+        Map {
+            dims: (terrain.len() as i32, terrain[0].len() as i32),
+            terrain,
+        }
+    }
+    fn get_height(&self, i: i32, j: i32) -> Option<&u8> {
+        self.terrain
+            .get(i as usize)
+            .and_then(|v: &Vec<_>| v.get(j as usize))
+    }
+
+    fn neighborhood(&self, i: i32, j: i32) -> impl Iterator<Item = ((i32, i32), u8)> + '_ {
+        [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            .iter()
+            .map(move |(di, dj)| (i + di, j + dj))
+            .filter_map(move |(i, j)| self.get_height(i, j).and_then(|h| Some(((i, j), *h))))
+    }
+}
 
 fn main() {
-    let map: Vec<Vec<u8>> = fs::read_to_string("map_t.txt")
-        .expect("file not found.")
-        .split('\n')
-        .map(|l| l.trim().chars().map(|c| c.to_digit(10).unwrap() as u8).collect())
-        .collect();
-    
-    let h = |i, j| if i < 0 || j < 0 { None } else { map.get(i as usize).and_then(|v: &Vec<_>| v.get(j as usize)) };
+    let map = Map::new(
+        fs::read_to_string("map.txt")
+            .expect("file not found.")
+            .lines()
+            .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as u8).collect())
+            .collect(),
+    );
 
     let mut low_points = vec![];
-    for i in 0..map.len() as i32 {
-        for j in 0..map[0].len() as i32 {
-            let curr = h(i, j).unwrap();
-            let surr = [h(i + 1, j), h(i - 1, j), h(i, j + 1), h(i, j - 1)];
-            let low_point = surr.iter().filter_map(|&h| h).all(|v| v > curr);
-            if low_point {
+    let (w, h) = map.dims;
+    for i in 0..w {
+        for j in 0..h {
+            let curr = map.get_height(i, j).unwrap();
+            if map.neighborhood(i, j).all(|(_, h)| h > *curr) {
                 low_points.push((i, j));
             }
         }
     }
 
-    let mut visited = HashSet::new();
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
     let mut neighbors = vec![];
+    let mut basins = vec![];
 
-    for low_point in low_points {
+    for &low_point in &low_points {
+        visited.clear();
         neighbors.push(low_point);
-        while neighbors.len() > 0 {
-            let pos = neighbors.pop();
-            
+        while let Some((i, j)) = neighbors.pop() {
+            for (p, h) in map.neighborhood(i, j) {
+                if h < 9 && !visited.contains(&p) {
+                    neighbors.push(p);
+                    visited.insert(p);
+                }
+            }
         }
+        basins.push(visited.len());
     }
 
-    println!("{:?}", low_points);
+    basins.sort();
+    let prod = basins.iter().rev().take(3).fold(1, |p, v| p*v);
+    println!("mult: {}", prod);
 }
